4196d6c5dcf88fe927ed5b4a80cb8de91aad700a
Michael Lynn <hailviral101@yahoo.com>
2019-11-06 15:37:22 -0700

    update
    update

---
+++ MemoryReview.c
@@ -0,0 +1,34 @@
+
+#define INITIAL_CAPACITY 3
+int main(){
+
+
+     int* arry = malloc(INITIAL_CAPACITY * sizeof(int));
+	 int k = 0;
+	 int n = INITIAL_CAPACITY;
+	 while(k < n)
+    {
+        arry[k] = k;
+		printf("%d""\n", arry[k]); // %s is format specifier
+        k++;
+    }
+	
+	 
+   char z[100] = "C Review.";
+   printf("%s""\n", z); // %s is format specifier
+   
+   
+	 
+	 char **array = malloc(INITIAL_CAPACITY * sizeof(char *));
+	 // Next you need to allocate space for each string:
+	 int i;
+	 int stringsize = 4;
+     for (i = 0; i < INITIAL_CAPACITY; ++i) {
+     array[i] = (char *)malloc(stringsize+1);
+	 array[i] = "test";
+	 printf("%s""\t", array[i]); // %s is format specifier
+	 }
+	 
+	 
+	 
+}
