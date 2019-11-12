2c4840fa66b6bfdb71c856ab7b722733197b6e2d
Michael Lynn <hailviral101@yahoo.com>
2019-11-11 20:32:58 -0700

    update
    update

---
+++ testing/StringFuzzBuzzAPI.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() 
+{			
+		char newString[] = "Hello World";
+		int count = 0;
+		for(int i = 0; newString[i] != '\0'; i++){
+			count = count + 1;
+			printf("%c", newString[i]);
+		
+		}
+		
+		strcat(newString,"!");
+		
+	    printf("\n");
+		 
+		printf("%s %d", newString, count);
+		
+		
+          return 0;
+}
+
