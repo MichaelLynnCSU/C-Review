c6d9afccb1556024675284db3285aa15af698d75
Michael Lynn <hailviral101@yahoo.com>
2019-11-11 20:00:21 -0700

    update

--- testing/LinkListBetterInsertionComplexity.c
+++ testing/LinkListBetterInsertionComplexity.c
@@ -6,26 +6,49 @@ struct student
            int id;
            char name[20];
            float percentage;
+		   struct student* node; 
 };
 
-void func(struct student *record);
 
 int main() 
 {
-          struct student record;
-
-          record.id=1;
-          strcpy(record.name, "Raju");
-          record.percentage = 86.5;
-
-          func(&record);
+          struct student* head = (struct student*)malloc(sizeof(struct student));
+		  struct student* front = (struct student*)malloc(sizeof(struct student));
+		  front = head;
+		  
+		  head->id=1;
+		  strcpy(head->name, "Raju");
+		  head->percentage = 86.5;
+		  
+		  
+		  int size = 5;	
+	      int count = 1;		  
+		  for(int i = 0; i < size; i++){
+			  count = count + 1;
+			  struct student* newNode = (struct student*)malloc(sizeof(struct student));
+			  newNode->id=count;
+			  strcpy(newNode->name, "Raju");
+			  newNode->percentage = 86.5;
+			  head->node = newNode;
+			  head =  head->node;
+			}
+		  
+		    head = front;
+		  	for(int i = 0; i < size; i++){
+				printf("%d\n", head->id);				
+				head = head->node;
+			}
+			
+			printf("\n");
+			
+		    head = front;
+		  	 for(int i = 0; i < size; i++){
+				if(i == 1){
+					head->id = 8;
+				}
+				printf("%d\n", head->id);				
+				head = head->node;
+			}
+		  
           return 0;
 }
-
-void func(struct student *record)
-{
-          printf(" Id is: %d \n", record->id);
-          printf(" Name is: %s \n", record->name);
-          printf(" Percentage is: %f \n", record->percentage);
-}
-		  
